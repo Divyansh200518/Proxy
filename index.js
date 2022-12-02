458acf0bca4ee9079bfe91a40207c2a081cb7257
Divyansh200518 <87868119+Divyansh200518@users.noreply.github.com>
2022-12-02 15:20:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const fetch = require('node-fetch')
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 
-async function getData(query) {
-    console.log(query)
-    const urlForAPI = `https://www.jiosaavn.com/api.php?__call=search.getResults&p=0&n=6&q=${query}&_format=json&_marker=00&ctx=wap6dot0`
-    const response = await fetch(urlForAPI)
-    const data = await response.json();
-    return data
-}
-
-const app = express();
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(cors());
-app.use(morgan('combined'));
-
-app.get('/:query', async (req, res) => {
-
-    const { query } = req.params;
-    var data = await getData(query)
-    res.status(200).send(data);
-});
-
-// starting the server
-app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
-    console.log(`listening on port ${process.env.PORT || 5000}`);
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function () {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
