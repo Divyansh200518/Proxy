63bdc64f0e1f257b93240edd36e492c76269a69f
Divyansh200518 <87868119+Divyansh200518@users.noreply.github.com>
2022-12-02 15:27:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ cors_proxy.createServer({
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(process.env.PORT || 5000, "0.0.0.0", function () {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+    console.log('Running CORS Anywhere on ' + "0.0.0.0" + ':' + process.env.PORT || 5000);
 });
